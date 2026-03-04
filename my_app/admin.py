7f551aad32bac92cb4f287c5eadc6967803c2103
vladislaav <parieiko.vlad@gmail.com>
2026-03-04 15:30:32 +0400

    Django lesson models: Register Book models through Admin class

--- my_app/admin.py
+++ my_app/admin.py
@@ -3,7 +3,34 @@ from django.contrib import admin
 from my_app.models import Book, Author, AuthorProfile, Post
 
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "price",
+        "category",
+        "published_date"
+    ]
+
+    search_fields = [
+        "title",
+    ]
+
+    list_filter = [
+        "published_date",
+        "category"
+    ]
+
+    list_editable = [
+        "category",
+        "price"
+    ]
+
+    list_per_page = 25
+
+
+# admin.site.register(Book, BookAdmin)
+
 admin.site.register(Author)
 admin.site.register(AuthorProfile)
 admin.site.register(Post)
